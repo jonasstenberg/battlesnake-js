2dc775a7ebc2edba181bf88c763158fd64893806
Ola Gawell <ola.gawell@jayway.com>
2019-05-22 22:12:50 +0200

    Completed test generator

--- test/test-generator.js
+++ test/test-generator.js
@@ -12,33 +12,6 @@ const writeToFile = (file, content) => {
   });
 };
 
-const fileContentStart = `const chai = require('chai');
-const buildRequest = require('./test-case-helper');
-const { start, end, move } = require('../game');
-
-const { expect } = chai;
-
-describe('Generated test case', () => {
-  beforeEach(() => {
-    start();
-  });
-
-  afterEach(() => {
-    end();
-  });
-
-  it('`;
-
-const fileContentEnd = `
-    move(body)
-      .then(result => {
-        expect(result.move).to.equal('left');
-        done();
-      })
-      .catch(done);
-  });
-});`;
-
 const printBoard = stringBoard => {
   let all = '';
   stringBoard.forEach(row => {
@@ -53,12 +26,16 @@ const printBoard = stringBoard => {
 
 const codeBoard = stringBoard => {
   let all = '';
-  stringBoard.forEach(row => {
-    let stringRow = `'`;
+  stringBoard.forEach((row, index) => {
+    let stringRow = `\t\t\t"`;
     row.forEach(character => {
       stringRow += character;
     });
-    all += `${stringRow}',\r\n`;
+
+    all += `${stringRow}"`;
+    if (index < stringBoard.length - 1) {
+      all += `,\r\n`;
+    }
   });
   return all;
 };
@@ -71,18 +48,18 @@ const rl = readline.createInterface({
 const inputPromise = new Promise(res => {
   rl.question('Paste the request body from wanted turn? ', bodyAnswer => {
     rl.question('Name of test case? ', nameAnswer => {
-      res({ nameInput: nameAnswer, bodyInput: bodyAnswer });
-      rl.close();
-      /* res({
-        nameInput: 'Simple test',
-        bodyInput:
-          '{"game":{"id":"f2019822-59f4-49c2-9a8d-42a10b495ba1"},"turn":6,"board":{"height":5,"width":5,"food":[{"x":2,"y":0},{"x":4,"y":2},{"x":4,"y":0},{"x":3,"y":4},{"x":3,"y":0},{"x":3,"y":2},{"x":4,"y":3},{"x":4,"y":1},{"x":3,"y":1},{"x":1,"y":0}],"snakes":[{"id":"6e1cb990-9e88-456a-bd48-bff203200137","name":"Test","health":100,"body":[{"x":3,"y":3},{"x":2,"y":3},{"x":1,"y":3},{"x":1,"y":2},{"x":1,"y":1},{"x":2,"y":1},{"x":2,"y":1}]},{"id":"bb20ce8d-2b03-409f-bd08-ec749f9b7286","name":"Test2","health":98,"body":[{"x":2,"y":4},{"x":1,"y":4},{"x":0,"y":4},{"x":0,"y":3},{"x":0,"y":2},{"x":0,"y":1}]}]},"you":{"id":"6e1cb990-9e88-456a-bd48-bff203200137","name":"Test","health":100,"body":[{"x":3,"y":3},{"x":2,"y":3},{"x":1,"y":3},{"x":1,"y":2},{"x":1,"y":1},{"x":2,"y":1},{"x":2,"y":1}]}}',
-      }); */
+      rl.question(
+        'Accepted directions, comma separeted (up, right, down, left)? ',
+        directionsAnswer => {
+          res({ nameInput: nameAnswer, bodyInput: bodyAnswer, directionsInput: directionsAnswer });
+          rl.close();
+        },
+      );
     });
   });
 });
 
-inputPromise.then(({ nameInput, bodyInput }) => {
+inputPromise.then(({ nameInput, bodyInput, directionsInput }) => {
   const body = JSON.parse(bodyInput);
 
   const board = [];
@@ -135,14 +112,32 @@ inputPromise.then(({ nameInput, bodyInput }) => {
     board[food.y][food.x] = '.';
   });
 
-  console.log(printBoard(board));
+  const directions = directionsInput.replace(/\s/g, '').split(',');
+  const directionsOutput = directions.map((d, index) => {
+    const val = `"${d}"`;
 
-  const output = `${fileContentStart}${nameInput}', done => {
-    // prettier-ignore
-    const body = buildRequest(100, [
+    if (index < directions.length - 1) {
+      return `${val},`;
+    }
+    return val;
+  });
+
+  const output = `
+  {
+    "board": [
 ${codeBoard(board)}
-    ]);
-    ${fileContentEnd}`;
+    ],
+    "name": "${nameInput}",
+    "acceptedOutcomes": [
+      ${directionsOutput}
+    ]
+  }`;
 
-  writeToFile(`test/generated-${nameInput.replace(/\s/g, '-').toLowerCase()}.test.js`, output);
+  writeToFile(
+    `test/generated/${nameInput
+      .replace(/\s/g, '-')
+      .replace(/\./g, '_')
+      .toLowerCase()}.json`,
+    output,
+  );
 });
